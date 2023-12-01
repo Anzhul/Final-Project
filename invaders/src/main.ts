3f0dff2403fb24b63c31e3a5e80c003d66579e6c
Anzhu Ling <anzhul@umich.edu>
2023-12-01 13:21:18 -0500

    Added three.js rendering

--- invaders/src/main.ts
+++ invaders/src/main.ts
@@ -1,6 +1,9 @@
 import './style.css'
-import * as THREE from 'three'; 
 import {Scene} from './scene.ts'
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { EXRLoader } from 'three/examples/jsm/loaders/EXRLoader.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 
 //Get stream data
@@ -9,11 +12,78 @@ import {Scene} from './scene.ts'
 
 console.log("hello world");
 let scene : Scene = new Scene("canvas");
-scene.setCameraPosition(0, 0, 5);
+scene.setCameraPosition(0, 0, 10);
+window.addEventListener('resize', () => {
+    console.log("window resized");
+    scene.setRenderSize(window.innerWidth, window.innerHeight);
+    scene.render();
+})
+
+let exrCubeRenderTarget;
+let exrBackground;
+
+const pmremGenerator = new THREE.PMREMGenerator( scene.renderer );
+pmremGenerator.compileEquirectangularShader();
+
+
+new EXRLoader().load('src/assets/moonlit_sky_0025.exr', function (texture: any) {
+
+    texture.mapping = THREE.EquirectangularReflectionMapping;
+
+	exrCubeRenderTarget = pmremGenerator.fromEquirectangular( texture );
+	exrBackground = texture;
+
+    scene.addBackground(exrBackground);
+    
+});
+
+
+// Instantiate a loader
+// Instantiate a loader
+const loader = new GLTFLoader();
+let camera: any;
+
+// Load a glTF resource
+loader.load(
+	// resource URL
+	'src/assets/camera.glb',
+	// called when the resource is loaded
+	function ( gltf ) {
+
+		scene.add( gltf.scene );
+        camera = gltf.cameras[ 0 ]
+
+	},
+	// called while loading is progressing
+	function ( xhr ) {
+
+		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+
+	},
+	// called when loading has errors
+);
 
 
 
 
+scene.render();
 //startLoop();
 
 
+const controls = new OrbitControls( scene.camera, scene.renderer.domElement );
+
+//controls.update() must be called after any manual changes to the camera's transform
+scene.camera.position.set( 0, 20, 100 );
+controls.update();
+
+function animate() {
+
+	requestAnimationFrame( animate );
+
+	// required if controls.enableDamping or controls.autoRotate are set to true
+	controls.update();
+
+	scene.renderer.render( scene.scene, camera );
+
+}
+animate();
--- invaders/src/scene.ts
+++ invaders/src/scene.ts
@@ -8,7 +8,7 @@ export class Scene{
         this.scene = new THREE.Scene();
         this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
         let canvas =   document.getElementById(renderElement)
-        this.renderer = new THREE.WebGLRenderer({ canvas: canvas as HTMLCanvasElement });
+        this.renderer = new THREE.WebGLRenderer({ canvas: canvas as HTMLCanvasElement, antialias: true });
         this.renderer.setSize( window.innerWidth, window.innerHeight );
     }
     setRenderSize(width: number, height: number){
@@ -19,6 +19,21 @@ export class Scene{
     }
     addMesh(mesh: THREE.Mesh){
         this.scene.add(mesh);
+    }
+    addLight(light: THREE.Light){
+        this.scene.add(light);
+    }
+    addGLTF(gltf: any){
+        this.scene.add(gltf.scene);
+    }
+    addBackground(background: THREE.Texture){
+        this.scene.background = background;
+    }
+    addEnvironment(environment: THREE.Texture){
+        this.scene.environment = environment;
+    }
+    add(obj: any){
+        this.scene.add(obj);
     }
     render(){
         this.renderer.render(this.scene, this.camera);
